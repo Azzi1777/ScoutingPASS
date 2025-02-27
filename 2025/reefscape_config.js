48235bc08bd2a6c470adf775eaca683193d369ca
Azzi1777 <701016831@gotvdsb.ca>
2025-02-27 15:05:17 -0500

    Update reefscape_config.js

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -47,6 +47,17 @@ var config_data = `
         "r3": "Red-3",
         "b3": "Blue-3"
       },
+       { "name": "Robot",
+      "code": "r",
+      "type": "robot",
+      "choices": {
+        "r1": "Red-1",
+        "b1": "Blue-1<br>",
+        "r2": "Red-2",
+        "b2": "Blue-2<br>",
+        "r3": "Red-3",
+        "b3": "Blue-3"
+      },
       "required":"true"
     },
     { "name": "Team #",
@@ -76,6 +87,14 @@ var config_data = `
     { "name": "Coral L4",
       "code": "ac4",
       "type": "counter"
+    },
+     { "name": "Auto Scoring Position",
+      "code": "asp",
+      "type": "clickable_image",
+      "filename": "2025/reef.png",
+      "dimensions": "6 6",
+      "allowableResponses": "1 2 3 4 5 6 7 8 9 10 11 12 13 14 17 18 19 20 23 24 25 26 27 28 29 30 31 32 33 34 35 36",
+      "shape": "circle 5 black red true"
     },
     { "name": "Processor Score",
       "code": "aps",
@@ -183,4 +202,4 @@ var config_data = `
       "type": "bool"
     }
   ]
-}`
+}`;
