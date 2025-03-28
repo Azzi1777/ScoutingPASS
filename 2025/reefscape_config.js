396352c536caa6adb6a7f9d29177cb647606dfa2
Azzi1777 <701016831@gotvdsb.ca>
2025-03-27 21:58:09 -0400

    Update reefscape_config.js

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "WATERLOO",
+      "defaultValue": "MCMASTER",
       "required": "true"
     },
     { "name": "Match Level",
